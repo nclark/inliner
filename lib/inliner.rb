f141a7a68b46e34f383aaf17e68f2272c4ab5815
Mark Sonnabaum <mark@sonnabaum.com>
2012-05-09 17:05:33 -0500

    Swiched http to eventmachine, removed minification for now as it's very slow.

--- lib/inliner.rb
+++ lib/inliner.rb
@@ -3,77 +3,116 @@ require 'uri'
 require 'net/http'
 require 'rack'
 require "inliner/version"
-require 'closure-compiler'
+require 'em-http-request'
+require 'pp'
 
 module Inliner
  class Inline
-    def initialize url
+    def initialize(url)
       unless validate_url(url)
         raise ArgumentError, "#{url} is not a valid URL!"
       end
 
+      @assets = {}
       @url = URI.parse(url)
-      @doc = Nokogiri::HTML(Net::HTTP.get(@url))
+      EM.run do
+        http = EM::HttpRequest.new(@url, :connect_timeout => 5, :inactivity_timeout => 10).get :redirects => 1
+        http.callback {
+          @doc = Nokogiri::HTML(http.response)
+          EM.stop
+        }
+      end
     end
 
     def validate_url(url)
       !(url =~ URI::regexp).nil?
     end
 
-    def inline_css
-      nodes = @doc.css('link[rel="stylesheet"][href]')
-      return false unless nodes.count > 0
-      nodes.each do |node|
-        file = file_for node
-        next if !file
+    def inline_asset(node)
+      contents = asset_contents(asset_uri(node))
+      return if !contents
+      node.before inline_node(contents, node)
+      node.remove
+    end
+
+    def inline_node(contents, node)
+      case node.name
+      when 'img'
+        inline = node.clone
+        inline['src'] = create_data_uri(asset_uri(node), contents)
+        inline['alt'] = node['alt'] if node['alt']
+      when 'link'
         inline = Nokogiri::XML::Node.new 'style', @doc
-        inline.content = file['contents']
-        node.before inline
-        node.remove
+        inline.content = inline_css_assets(contents)
+      else
+        inline = Nokogiri::XML::Node.new node.name, @doc
+        inline.content = contents
       end
+      inline
     end
 
-    def inline_js
-      nodes = @doc.css('script[src]')
-      return false unless nodes.count > 0
-      nodes.each do |node|
-        file = file_for node
-        next if !file
-        inline = Nokogiri::XML::Node.new 'script', @doc
-        inline.content = Closure::Compiler.new.compile file['contents']
-        node.before inline
-        node.remove
+    def inline_css_assets(contents)
+      css_urls = Hash[contents.scan(/url\([\'"]?(.+?)[\'"]?\)/).map { |u|
+        uri = @url + u[0]
+        [u[0], uri.to_s]
+      }]
+
+      unless css_urls.empty?
+        get_urls(css_urls.values)
+        css_urls.each do |path, full|
+          data_uri = create_data_uri(path, asset_contents(full))
+          contents = contents.gsub(path, data_uri)
+        end
       end
+      contents
     end
 
-    def inline_images
-      nodes = @doc.css('img')
-      return false unless nodes.count > 0
-      nodes.each do |node|
-        file = file_for node
-        next if !file
-        img = node.clone
-        ext = File.extname(file['path'])
-        img['src'] = "data:#{Rack::Mime.mime_type(File.extname(file['path']))};base64,#{[file['contents']].pack('m')}"
-        img['alt'] = node['alt'] if node['alt']
-        node.before img
-        node.remove
-      end
+    def create_data_uri(path, contents)
+      mime = Rack::Mime.mime_type(File.extname(path))
+      "data:#{Rack::Mime.mime_type(File.extname(path))};base64,#{[contents].pack('m').gsub(/\n/, "")}"
     end
 
     def to_html
       return @doc.to_s
     end
 
+    def get_urls(urls)
+      assets = {}
+      EventMachine.run do
+        multi = EventMachine::MultiRequest.new
+        urls.uniq.each_with_index do |url, idx|
+          begin
+            http = EventMachine::HttpRequest.new(url, :connect_timeout => 1)
+            req = http.get :redirects => 1
+          rescue URI::InvalidURIError(e)
+            p e.backtrace
+          end
+          multi.add url, req
+        end
+
+        multi.callback do
+          multi.responses[:callback].each do |idx, r|
+            #p idx
+            assets[idx] = r.response
+          end
+          EventMachine.stop
+        end
+      end
+      assets
+    end
+
     def inline
-      inline_css
-      inline_js
-      inline_images
+      assets = @doc.css('img') + @doc.css('link[rel="stylesheet"][href]') + @doc.css('script[src]')
+      urls = assets.map {|node| asset_uri(node)}
+
+      @assets = get_urls urls
+      assets.each do |node|
+        inline_asset node
+      end
       to_html
     end
 
-    private
-    def file_for node
+    def asset_uri(node)
       path = case node.name
       when 'script'
         node['src']
@@ -84,12 +123,12 @@ module Inliner
       end
       return false unless path
 
-      # Catch relative urls
-      if path.index('/') == 0
-        path = @url.to_s.sub(@url.path, path)
-      end
-      contents = Net::HTTP.get(URI.parse(path))
-      {'contents' => contents, 'path' => path}
+      uri = @url + path
+      uri.to_s
+    end
+
+    def asset_contents(uri)
+      @assets[uri] || Net::HTTP.get(URI.parse(uri))
     end
   end
 end
